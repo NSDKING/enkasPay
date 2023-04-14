f8182f9cf0f15cb693b31839406def71240d94ca
dimitri Nzeulie <80715794+NSDKING@users.noreply.github.com>
2023-04-14 16:03:17 +0100

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,25 @@ import DefaultButton from '../components/DefaultButton'
 import './css/home.css'
 import om from './img/om.png'
 import mtn from './img/mtn.png'
+import mobile from './img/mobileFronts.png'
 
 export default function HomePage() {
     return(
         <section className="homepage">
             <Navbar/>
+            <div className="marge"></div>
+            <img src={mobile} width="100%" />
+            <div className='midle-section'>
+                <h2>un seul site pour</h2>
+                <h2>effectuer tes achats</h2> 
+                <h2>en ligne</h2> 
+
+            </div>
+            <div className='midle-section2'>
+            <p>avec enkas pay effectuez tous vos achats en lignes sans carte bancaires, de l'achat a la livraison.Ouvrez votre compte en un clin d'oeil</p>
+            </div>
+            <DefaultButton text={"obtenir un compte gratuit"} bgcolor={"rgb(251, 61, 61)"} textcolor={"white"} width={"60%"} height={"35px"} marginTop={"20px"} />
+
             <section className="home-box" id="box1">
                 <p className="p-home">enkas pay te permet de faire tes achats en ligne simplement et en toutes securité</p>
             </section>  
