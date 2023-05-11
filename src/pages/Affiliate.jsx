4eeead0b7a1f06042fca944b7d2ae99f957a1e2d
dimitri Nzeulie <80715794+NSDKING@users.noreply.github.com>
2023-05-11 08:46:06 +0100

    correction

--- src/pages/Affiliate.jsx
+++ src/pages/Affiliate.jsx
@@ -13,44 +13,44 @@ export default function AffiliatePage() {
                 </div>
                 <div className="affiliatepage-title">
                     <h1 >
-                        devient un  partenaires affiliés
+                        devient un  partenaires affilié
                     </h1>
                 </div>
                 <p className="affiliatepage-first-text">
-                    aide nous a promouvoir enkaspay et gagne des commisssions grace a ton code partenaire
-                </p>
+                    aide nous a promouvoir enkaspay et gagne des commissions grace à ton code partenaire               
+                 </p>
                 <DefaultButtonLink text={"obtenir mon code partenaire"} bgcolor={"#eb0625"} textcolor={"white"} width={"90%"} height={"50px"} marginTop={"10px"} location={"/login"}/>
             </div>
             <div className="affiliatepage-body">
-                <h1>comment ca marche?</h1>
+                <h1>comment ça marche?</h1>
                 <div className="textBox">    
-                    <Textbox text={" c’est tres simple cliques sur obtenir mon code partenaires puis entre les informations apres cela tu recevras ton code partenaire et tu n’aura plus qu’a le partager et chaque utilisation tu recois ton due."}/>
+                    <Textbox text={"c’est très simples cliques sur obtenir mon code partenaires puis entre les informations après cela tu recevras ton code partenaire et tu n’auras plus qu’a le partagé et chaque utilisation tu reçois ta due."}/>
 
                 </div>
-                <h1>comment est ce que je recois mon argent ?</h1>
+                <h1>comment est-ce que je reçois mon argent ?</h1>
                 <div className="textBox">    
 
-                    <Textbox text={"tu recois ton argent par om ou momo chaque samedi a ta demande. pour le faire clique sur le bouton affiliation dans le menu "}/>
+                    <Textbox text={"tu reçois ton argent par om où Momo chaque samedi a ta demande. pour le faire clique sur le bouton affiliation dans le menu"}/>
                 </div>
-                <h1>ou est ce que je peux consulter mon solde?</h1>
+                <h1>Où est-ce que je peux consulter ma solde?</h1>
 
                 <div className="textBox">    
 
-                    <Textbox text={"tu peux consulter ton solde le nombre de personnes qui ont utiliser ton code partenaire sur ce site et meme les produits qu’ils ont acheté si tu deviens commerciale"}/>
+                    <Textbox text={"tu peux consulter ton solde, le nombre de personnes qui ont utilisé ton code partenaire sur ce site et même les produits qu’ils ont achetés si tu deviens commerciale"}/>
                 </div>
 
                 <h1>comment devenir commercial?</h1>
 
                 <div className="textBox">    
 
-                    <Textbox text={"pour devenir un commercioal c’est tres simple ton code partenaire doit etre utiliser plus de 7 fois par semaine"}/>
+                    <Textbox text={"pour devenir un commercial c’est très simple ton code partenaire doit être utilisé plus de 7 fois par semaine"}/>
                 </div>
                 
-                <h1>quel sont les avantages a etre un commerciales ?</h1>
+                <h1>Quels sont les avantages à être une commerciale ?</h1>
 
                 <div className="textBox">    
 
-                    <Textbox text={"un commercial gagne plus qu’un partenaire pour chaque utilisation de son code partenaire et integre un groupe ou il benefiecie de l'experience des autres membres."}/>
+                    <Textbox text={"un commercial gagne plus qu’un partenaire pour chaque utilisation de son code partenaire et intègre un groupe ou il benefiecie de l'expérience des autres membres."}/>
                 </div>
 
              </div>
